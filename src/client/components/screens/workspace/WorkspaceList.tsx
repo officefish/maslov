0b9a35dcc80d12316540de0ea9bd70aec7a121f8
officefish <indicogames@yandex.ru>
2024-01-23 13:49:46 -0800

    new workspace modal form added

--- src/client/components/screens/workspace/WorkspaceList.tsx
+++ src/client/components/screens/workspace/WorkspaceList.tsx
@@ -1,19 +1,66 @@
-import { FC } from 'react'
+import { FC, useRef, MouseEvent } from 'react'
+import useGlobalOverflowHidden from '@client/hooks/force-overflow'
 
 import {
   StyledButton,
+  StyledCancelButton,
   StyledWorkspace,
   StyledWorkspaceGrid,
+  StyledDialog,
+  StyledForm,
+  StyledFormWrapper,
+  StyledFormHeader,
 } from './workspace.styled'
 
 const WorkspacesList: FC = () => {
+  const modalRef = useRef<HTMLDialogElement>(null)
+
+  /* body overflow: hidden style controller */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [isOverflowHidden, setIsOverflowHidden] = useGlobalOverflowHidden()
+  const onDialogClose = () => setIsOverflowHidden(false)
+
+  const showNewWorkspaceModal = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    if (modalRef && modalRef.current) {
+      setIsOverflowHidden(true)
+      const modal = modalRef.current
+      modal.showModal()
+    }
+  }
+
+  function cancelWorkspaceCration(e: MouseEvent<HTMLButtonElement>): void {
+    e.preventDefault()
+    if (modalRef && modalRef.current) {
+      const modal = modalRef.current
+      modal.close()
+    }
+    //throw new Error('Function not implemented.')
+  }
+
   return (
-    <StyledWorkspaceGrid>
-      <StyledWorkspace>IBM daily</StyledWorkspace>
-      <StyledWorkspace>AMAZON weekly</StyledWorkspace>
-      <StyledWorkspace>CISCO vs Huawey</StyledWorkspace>
-      <StyledButton>Add workspace</StyledButton>
-    </StyledWorkspaceGrid>
+    <>
+      <StyledWorkspaceGrid>
+        <StyledWorkspace>IBM daily</StyledWorkspace>
+        <StyledWorkspace>AMAZON weekly</StyledWorkspace>
+        <StyledWorkspace>CISCO vs Huawey</StyledWorkspace>
+        <StyledButton onClick={showNewWorkspaceModal}>
+          Add workspace
+        </StyledButton>
+      </StyledWorkspaceGrid>
+      <StyledDialog ref={modalRef} onClose={onDialogClose}>
+        <StyledForm method="dialog">
+          <StyledFormWrapper>
+            <StyledFormHeader>New workspace</StyledFormHeader>
+            <div>
+              <StyledCancelButton onClick={cancelWorkspaceCration}>
+                Cancel
+              </StyledCancelButton>
+            </div>
+          </StyledFormWrapper>
+        </StyledForm>
+      </StyledDialog>
+    </>
   )
 }
 export default WorkspacesList
--- src/client/components/screens/workspace/workspace.styled.tsx
+++ src/client/components/screens/workspace/workspace.styled.tsx
@@ -8,6 +8,38 @@ export const StyledButton = tw.button`
 btn btn-outline btn-primary h-24 gap-2 uppercase
 `
 
+export const StyledCancelButton = tw.button`
+btn btn-outline btn-error gap-2 uppercase
+`
+
 export const StyledWorkspace = tw.button`
 btn btn-outline btn-secondary h-24 gap-2
+`
+
+export const StyledDialog = tw.dialog`
+modal
+`
+
+export const StyledForm = tw.form`
+!w-11/12 !max-w-5xl !h-[80%] 
+modal-box
+flex flex-col items-center justify-center
+`
+
+export const StyledFormWrapper = tw.div`
+card max-w-[96%] md:max-w-[70%] shadow-2xl flex flex-col items-center bg-base-300 dark:bg-base-300-dark h-[70%] w-full
+`
+export const StyledFormHeader = tw.h2`
+w-full 
+h-14 
+inline-flex 
+items-center 
+justify-center 
+font-bold 
+rounded-t-lg
+uppercase
+bg-accent 
+text-accent-content
+dark:bg-accent-dark
+dark-text-accent-content-dark
 `
